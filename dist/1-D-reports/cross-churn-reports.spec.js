3c2a1cb8d337dd5d1cdb5a89047cb437e9994088
Enrico PIccinin <enrico.piccinin@gmail.com>
2023-05-08 12:41:28 +0200

    1.1.10

--- dist/1-D-reports/cross-churn-reports.spec.js
+++ dist/1-D-reports/cross-churn-reports.spec.js
@@ -86,5 +86,41 @@ describe(`fileChurnReport compare with authorChurnReport`, () => {
             complete: () => done(),
         });
     }).timeout(20000);
+    it(`Generates the reports and verifies that the total churn for authorChurnReport is equal to the one for fileChurnReport. 
+    The churn is the the same because fileChurnReport considers also the files with cloc zero.
+    Therefore also the churn of the files that are not currently in the project is considered, which is the same logic
+    applied when calculating the authorChurnReport`, (done) => {
+        const repoName = 'kafka-commits-reverse-1.gitlog';
+        const commitLogPath = path_1.default.join(process.cwd(), `/test-data/output/${repoName}`);
+        const clocLogPath = path_1.default.join(process.cwd(), `/test-data/output/kafka-cloc.csv`);
+        const outDir = `${process.cwd()}/temp`;
+        const ignoreClocZero = false;
+        // build the file churn report
+        const fileCommits = (0, files_1.filesStream)(commitLogPath, clocLogPath);
+        const fileChurns = (0, file_churn_aggregate_1.fileChurn)(fileCommits, ignoreClocZero);
+        const fParams = {
+            commitLog: commitLogPath,
+            outDir,
+        };
+        const fChurnReport = (0, file_churn_report_1.fileChurnReportCore)(fileChurns, fParams);
+        // build the author churn report
+        const commits = (0, commits_1.commitsStream)(commitLogPath);
+        const authorChurns = (0, author_churn_aggregate_1.authorChurn)(commits);
+        const params = {
+            commitLog: commitLogPath,
+            outDir,
+        };
+        const authChurnReport = (0, author_churn_report_1.authorChurnReportCore)(authorChurns, params);
+        (0, rxjs_1.forkJoin)([fChurnReport, authChurnReport])
+            .pipe((0, rxjs_1.tap)((reports) => {
+            const fReport = reports[0];
+            const aReport = reports[1];
+            (0, chai_1.expect)(fReport.totChurn.val).equal(aReport.totChurn.val);
+        }))
+            .subscribe({
+            error: (err) => done(err),
+            complete: () => done(),
+        });
+    }).timeout(20000);
 });
 //# sourceMappingURL=cross-churn-reports.spec.js.map
