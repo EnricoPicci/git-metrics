229491f9f15e9c4016fb77bec2880822d659797d
Enrico Piccinin <enrico.piccinin@gmail.com>
2023-12-14 15:23:22 +0100

    isolate codeTurnover into a file

---
+++ src/git-cloc-functions/code-turnover.ts
@@ -0,0 +1,19 @@
+
+//********************************************************************************************************************** */
+//****************************   APIs                               **************************************************** */
+//********************************************************************************************************************** */
+
+import { WriteClocDiffWithCommitForReposOptions, writeClocDiffWithCommitForRepos$ } from "./cloc-diff-commit";
+
+/**
+ * Calculates the code turnover for a set of repositories and returns an Observable that emits when the operation is complete.
+ * @param folderPath The path to the folder containing the repositories.
+ * @param options An object containing options for the operation. Defaults to an empty object.
+ * @returns An Observable that emits when the operation is complete.
+ */
+export function codeTurnover$(
+    folderPath: string,
+    options: WriteClocDiffWithCommitForReposOptions = {}
+) {
+    return writeClocDiffWithCommitForRepos$(folderPath, options)
+}
